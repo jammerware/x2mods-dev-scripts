49675eb58c2a782512eec4a25bd603f8b92adb5f
ben <ben@jammerware.com>
2017-10-19 10:56:54 -0400

    Added the switch to the unreal editor script to suppress the Game Asset Database warning.

--- scripts/runUnrealEditor.ps1
+++ scripts/runUnrealEditor.ps1
@@ -2,4 +2,4 @@ Param(
     [string]$gamePath # the path to your XCOM 2 installation ending in "XCOM 2"
 )
 
-& "$gamePath/Binaries/Win64/XComGame.exe" editor -noscriptcompile
+& "$gamePath/Binaries/Win64/XComGame.exe" editor -noscriptcompile -NoGADWarning
